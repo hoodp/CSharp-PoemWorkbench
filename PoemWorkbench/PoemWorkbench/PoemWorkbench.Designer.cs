612dc2d30fc92aeeebc19e4d1e71db2f4660ac3c
hoodp <hoodp@mail.gvsu.edu>
2014-04-30 16:57:56 -0400

    Uploaded C# Project

---
+++ PoemWorkbench/PoemWorkbench/PoemWorkbench.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace PoemWorkbench
+{
+    partial class PoemWorkbench
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressText = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grpColor = new System.Windows.Forms.GroupBox();
+            this.radGreen = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.radRed = new System.Windows.Forms.RadioButton();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPoem = new System.Windows.Forms.TextBox();
+            this.btnClearLast = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAddLine = new System.Windows.Forms.Button();
+            this.listBoxPoem = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.grpColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressText
+            // 
+            this.progressText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressText.Location = new System.Drawing.Point(361, 44);
+            this.progressText.Maximum = 50;
+            this.progressText.Name = "progressText";
+            this.progressText.Size = new System.Drawing.Size(82, 13);
+            this.progressText.Step = 1;
+            this.progressText.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Max. Characters: 50";
+            // 
+            // grpColor
+            // 
+            this.grpColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpColor.Controls.Add(this.radGreen);
+            this.grpColor.Controls.Add(this.radBlue);
+            this.grpColor.Controls.Add(this.radRed);
+            this.grpColor.Location = new System.Drawing.Point(458, 61);
+            this.grpColor.Name = "grpColor";
+            this.grpColor.Size = new System.Drawing.Size(85, 86);
+            this.grpColor.TabIndex = 20;
+            this.grpColor.TabStop = false;
+            this.grpColor.Text = "Background:";
+            // 
+            // radGreen
+            // 
+            this.radGreen.AutoSize = true;
+            this.radGreen.Location = new System.Drawing.Point(7, 65);
+            this.radGreen.Name = "radGreen";
+            this.radGreen.Size = new System.Drawing.Size(54, 17);
+            this.radGreen.TabIndex = 2;
+            this.radGreen.TabStop = true;
+            this.radGreen.Text = "Green";
+            this.radGreen.UseVisualStyleBackColor = true;
+            this.radGreen.Click += new System.EventHandler(this.radGreen_Click);
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.Location = new System.Drawing.Point(6, 43);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(46, 17);
+            this.radBlue.TabIndex = 1;
+            this.radBlue.TabStop = true;
+            this.radBlue.Text = "Blue";
+            this.radBlue.UseVisualStyleBackColor = true;
+            this.radBlue.Click += new System.EventHandler(this.radBlue_Click);
+            // 
+            // radRed
+            // 
+            this.radRed.AutoSize = true;
+            this.radRed.Location = new System.Drawing.Point(7, 20);
+            this.radRed.Name = "radRed";
+            this.radRed.Size = new System.Drawing.Size(45, 17);
+            this.radRed.TabIndex = 0;
+            this.radRed.TabStop = true;
+            this.radRed.Text = "Red";
+            this.radRed.UseVisualStyleBackColor = true;
+            this.radRed.Click += new System.EventHandler(this.radRed_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(458, 311);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(85, 30);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Enter Line:";
+            // 
+            // txtPoem
+            // 
+            this.txtPoem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPoem.Location = new System.Drawing.Point(34, 61);
+            this.txtPoem.MaxLength = 55;
+            this.txtPoem.Name = "txtPoem";
+            this.txtPoem.Size = new System.Drawing.Size(409, 20);
+            this.txtPoem.TabIndex = 12;
+            this.txtPoem.TextChanged += new System.EventHandler(this.txtPoem_TextChanged);
+            // 
+            // btnClearLast
+            // 
+            this.btnClearLast.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClearLast.Location = new System.Drawing.Point(119, 311);
+            this.btnClearLast.Name = "btnClearLast";
+            this.btnClearLast.Size = new System.Drawing.Size(85, 30);
+            this.btnClearLast.TabIndex = 15;
+            this.btnClearLast.Text = "Clear &Last";
+            this.btnClearLast.UseVisualStyleBackColor = true;
+            this.btnClearLast.Click += new System.EventHandler(this.btnClearLast_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFont.Location = new System.Drawing.Point(458, 153);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(85, 30);
+            this.btnFont.TabIndex = 18;
+            this.btnFont.Text = "&Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Location = new System.Drawing.Point(204, 311);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 30);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAddLine
+            // 
+            this.btnAddLine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAddLine.Location = new System.Drawing.Point(34, 311);
+            this.btnAddLine.Name = "btnAddLine";
+            this.btnAddLine.Size = new System.Drawing.Size(85, 30);
+            this.btnAddLine.TabIndex = 14;
+            this.btnAddLine.Text = "&Add Line";
+            this.btnAddLine.UseVisualStyleBackColor = true;
+            this.btnAddLine.Click += new System.EventHandler(this.btnAddLine_Click);
+            // 
+            // listBoxPoem
+            // 
+            this.listBoxPoem.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPoem.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listBoxPoem.FormattingEnabled = true;
+            this.listBoxPoem.HorizontalScrollbar = true;
+            this.listBoxPoem.Location = new System.Drawing.Point(34, 87);
+            this.listBoxPoem.Name = "listBoxPoem";
+            this.listBoxPoem.ScrollAlwaysVisible = true;
+            this.listBoxPoem.Size = new System.Drawing.Size(409, 173);
+            this.listBoxPoem.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Poet Workbench";
+            // 
+            // PoemWorkbench
+            // 
+            this.AcceptButton = this.btnAddLine;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 346);
+            this.Controls.Add(this.progressText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.grpColor);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPoem);
+            this.Controls.Add(this.btnClearLast);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAddLine);
+            this.Controls.Add(this.listBoxPoem);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(590, 384);
+            this.Name = "PoemWorkbench";
+            this.Text = "Poem Workshop";
+            this.grpColor.ResumeLayout(false);
+            this.grpColor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox grpColor;
+        private System.Windows.Forms.RadioButton radGreen;
+        private System.Windows.Forms.RadioButton radBlue;
+        private System.Windows.Forms.RadioButton radRed;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPoem;
+        private System.Windows.Forms.Button btnClearLast;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAddLine;
+        private System.Windows.Forms.ListBox listBoxPoem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ PoemWorkbench/PoemWorkbench/PoemWorkbench.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+/*
+ * Paul Hood
+ * Resubmitted -- 5/1/2013
+ * Lesson 1
+ * No Questions asked policy
+*/
+namespace PoemWorkbench
+{
+    public partial class PoemWorkbench : Form
+    {
+        public PoemWorkbench()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+        
+        // event to add line
+        private void btnAddLine_Click(object sender, EventArgs e)
+        {
+            AddLine();
+        }
+
+        // font button used for changing fonts
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = listBoxPoem.Font; // set fontdialog to listbox font
+            fontDialog1.Color = listBoxPoem.ForeColor; // set font to listbox color
+
+            // if font dialog is changed and OK
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBoxPoem.Font = fontDialog1.Font; // change font
+                listBoxPoem.ForeColor = fontDialog1.Color; // change color
+            }
+        }
+
+        // EDITED -- text is changed inside textbox
+        private void txtPoem_TextChanged(object sender, EventArgs e)
+        {
+            if (!(CheckLength())) // if CheckLength is not true
+            {
+                AddLine(); // add the line 
+            }
+            else
+            {
+                progressText.Value = line(txtPoem).Length; // for progress bar
+            }
+        }
+
+        // click event for red
+        private void radRed_Click(object sender, EventArgs e)
+        {
+            changeColor(Color.Red);
+        }
+
+        // click event for blue
+        private void radBlue_Click(object sender, EventArgs e)
+        {
+            changeColor(Color.Blue);
+        }
+
+        // click event for green
+        private void radGreen_Click(object sender, EventArgs e)
+        {
+            changeColor(Color.Green);
+        }
+
+        // button click for clearing all text
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // confirmation
+            if (MessageBox.Show("This will clear all current data! Are you sure you wish to continue?", 
+                    "Alert",MessageBoxButtons.YesNo,MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                txtPoem.Clear(); // clear textbox
+                listBoxPoem.Items.Clear(); // clear all items in listbox             
+            }
+            txtPoem.Focus(); // send focus
+        }
+
+        // events for clearing last item
+        private void btnClearLast_Click(object sender, EventArgs e)
+        {
+            // if count is greater than 0 - prevents error
+            if (listBoxPoem.Items.Count > 0)
+            {
+                // remove at couunt - 1
+                listBoxPoem.Items.RemoveAt(listBoxPoem.Items.Count - 1);
+            }
+            else // message box cleared
+            {
+                MessageBox.Show("You currently have no lines in your poem.", "Poem Clear", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            txtPoem.Focus(); // send focus
+        }
+
+        // button click for exit
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // exit application
+        }
+        #endregion
+
+        #region Methods
+        // CHANGED -- added method for changed backcolor of listbox
+        private void changeColor(Color color)
+        {
+            // check to see if color matches any from radiobox
+            if (color == Color.Red || color == Color.Blue
+                    || color == Color.Green)
+            {
+                // if color is green change font color to white for better visibility
+                if (color == Color.Green)
+                {
+                    listBoxPoem.ForeColor = Color.White;
+                }
+                else // change color to black
+                {
+                    listBoxPoem.ForeColor = Color.Black;
+                }
+                listBoxPoem.BackColor = color; // set backcolor to radio button
+            }
+            else // set back to white if there is some sort of error
+            {
+                listBoxPoem.BackColor = Color.White;
+            }
+        }
+
+        // ADDED return string of text in the box
+        public string line(TextBox box)
+        {
+            return box.Text.Trim(); // return trimmed textbox info
+        }
+
+        // ADDED check to see if data is valid
+        public bool CheckTextBox()
+        {
+            // if textbox length is greater than 0
+            if (line(txtPoem).Length > 0)
+            {
+                return true;
+            }
+            else // error message
+            {
+                MessageBox.Show("Please enter valid line into textbox.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtPoem.Clear(); // clear to check to blank space
+                txtPoem.Focus(); // send focus
+            }
+            return false;
+        }
+
+        // ADDED method for adding line and updating info
+        public void AddLine()
+        {
+            // check to see if valid
+            if (CheckTextBox())
+            {
+                listBoxPoem.Items.Add(line(txtPoem)); // add line
+                progressText.Value = 0; // reset progress value
+                txtPoem.Clear(); // clear textbox
+                txtPoem.Focus(); // send focus to textbox
+            }
+        }
+
+        // EDITED -- method for checking length
+        public bool CheckLength()
+        {
+            if (line(txtPoem).Length < 50) // if length is less than 50
+            {
+                return true;
+            }
+            else // more than 50
+            {
+                return false;
+            }
+        }  
+        #endregion   
+    }
+}
